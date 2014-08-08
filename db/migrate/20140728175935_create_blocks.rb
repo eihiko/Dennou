61a7dcdb437a7996f0e061c5a4cacffe0efb875c
oeihiko <oeihiko@gmail.com>
2014-08-08 15:19:05 -0400

    Create users migration

--- db/migrate/20140728175935_create_blocks.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlocks < ActiveRecord::Migration
-  def change
-    create_table :blocks do |t|
-      t.int :handle_id
-      t.references :content, polymorphic: true, index: true
-    end
-  end
-end
